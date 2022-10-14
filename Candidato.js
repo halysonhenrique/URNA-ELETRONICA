2d91b49105e53ffb1205ce4d7a96863d7f9fbacf
Perkles <halybeatriz@gmail.com>
2022-10-14 18:04:29 -0300

    URNA

---
+++ Candidato.js
@@ -0,0 +1,10 @@
+ class Candidato {
+    constructor(nome, numero, img) {
+        this.nome = nome
+        this.numero = numero
+        this.img = img
+    }
+}
+
+  let candidato1 = new Candidato('Jair Messias Bolsonaro', '22', './imagens/Bolso.jpg')
+  let candidato2 = new Candidato('LULINHA', '13', './imagens/LULA.jpeg')
